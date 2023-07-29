466687f7f6ebcfb78b8cb2ccd3d94342c556ad75
cardioswapper <admin@cardioswap.io>
2023-07-29 16:07:42 +0200

    change colors

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -4233,7 +4233,7 @@ var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { ba
         blue: "linear-gradient(180deg, #A7E8F1 0%, #94E1F2 100%)",
         violet: "linear-gradient(180deg, #E2C9FB 0%, #CDB8FA 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#6FC7DA", background: "#100C18", backgroundDisabled: "#3c3742", backgroundAlt: "#27262c", contrast: "#FFFFFF", dropdown: "#1E1D20", invertedContrast: "#191326", input: "#6FC7DA", inputSecondary: "#6FC7DA", primaryDark: "#2B4B9B", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#6FC7DA", borderColor: "#524B63", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#7645D9", background: "#100C18", backgroundDisabled: "#3c3742", backgroundAlt: "#27262c", contrast: "#FFFFFF", dropdown: "#1E1D20", invertedContrast: "#191326", input: "#483f5a", inputSecondary: "#66578D", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#A28BD4", borderColor: "#524B63", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
         cardHeader: "linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)",
         blue: "linear-gradient(180deg, #00707F 0%, #19778C 100%)",
